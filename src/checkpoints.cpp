3dccabcfd38e3a761d9e7ffb33d8494dc4e58655
Franko <contact@frankos.org>
2013-09-10 09:46:47 -0400

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -39,6 +39,7 @@ namespace Checkpoints
             ( 70000, uint256("0x01ad0aa03e8888cef8381aeea5ca679602bbb32e8245e9cd36abce77cda936bfb"))
             ( 80000, uint256("0x03a508431391d8203d4dd8fde8ffc529c26923cc8da9f2e64342441bb7afa940c"))
             ( 90000, uint256("0x08571865fb72beddf95872cbe3490aeb2b7558c7810b0a4aef9d581e249ef9d98"))
+            ( 216802, uint256("0x0c946c4bdb51a240a103059f69112f301995e3a293044d3a4eff8d4c95cb0a5e1"))
             ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
